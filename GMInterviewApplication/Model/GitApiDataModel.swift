53959187dde8831cd18d7dd281b3223fc23e135b
Pranoti <prkulkarni525@gmail.com>
2020-12-21 21:07:58 -0500

    Added Intia model and view classes as per MVC architecture

---
+++ GMInterviewApplication/Model/GitApiDataModel.swift
@@ -0,0 +1,21 @@
+//
+//  GitApiDataModel.swift
+//  GMInterviewApplication
+//
+//  Created by PRANOTI KULKARNI on 12/21/20.
+//
+
+import UIKit
+
+struct UserData: Codable {
+    let commit : Commit?
+    
+    struct Commit: Codable {
+        let author: Author?
+        let message: String?
+    }
+    
+    struct Author: Codable {
+        let name: String?
+    }
+}
